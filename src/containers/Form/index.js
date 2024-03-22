e38b09a008596b7a512982d7f91ac3ec95c41222
Karim Dahmani <karim.dahmani88@gmail.com>
2024-03-22 13:39:49 +0100

    Add required attribute to form fields

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -8,6 +8,7 @@ const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
+
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
@@ -29,16 +30,17 @@ const Form = ({ onSuccess, onError }) => {
     <form onSubmit={sendContact}>
       <div className="row">
         <div className="col">
-          <Field placeholder="" label="Nom" />
-          <Field placeholder="" label="Prénom" />
+          <Field placeholder="" label="Nom" required/>
+          <Field placeholder="" label="Prénom" required/>
           <Select
             selection={["Personel", "Entreprise"]}
             onChange={() => null}
             label="Personel / Entreprise"
             type="large"
             titleEmpty
+            required
           />
-          <Field placeholder="" label="Email" />
+          <Field placeholder="" label="Email" required/>
           <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
             {sending ? "En cours" : "Envoyer"}
           </Button>
@@ -48,6 +50,7 @@ const Form = ({ onSuccess, onError }) => {
             placeholder="message"
             label="Message"
             type={FIELD_TYPES.TEXTAREA}
+            required
           />
         </div>
       </div>
